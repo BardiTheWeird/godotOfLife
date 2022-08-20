592040c23bd2047e827998d3f6e012e1168b3e95
Oleksii Dobrovolskyi <bardianofmandalore@gmail.com>
2022-08-20 14:52:26 +0300

    zoom; better ui

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,9 +1,21 @@
 extends Camera2D
 
+export var zoom_step := 0.1 setget set_zoom_step
+var zoom_stepv : Vector2
+
+func set_zoom_step(value: float) -> void:
+	zoom_step = value
+	zoom_stepv = Vector2.ONE * zoom_step
+	print('zoom_stepv: %s' % zoom_stepv)
+
+
 var mouse_start_pos
 var screen_start_position
 
 var dragging = false
+
+func _ready():
+	self.zoom_step = zoom_step
 
 func _input(event):
 	if event.is_action("drag"):
@@ -15,3 +27,9 @@ func _input(event):
 			dragging = false
 	elif event is InputEventMouseMotion and dragging:
 		position = zoom * (mouse_start_pos - event.position) + screen_start_position
+	elif event.is_action("zoom_in"):
+		print('zooming in')
+		zoom -= zoom_stepv
+	elif event.is_action("zoom_out"):
+		print('zooming out')
+		zoom += zoom_stepv
--- Tile.gd
+++ Tile.gd
@@ -4,8 +4,6 @@ class_name Tile
 export (Color) var alive_color
 export (Color) var dead_color
 
-onready var tween : Tween = $Tween
-
 var alive : bool = false
 
 func _ready():
@@ -13,20 +11,27 @@ func _ready():
 
 func set_dead():
 	alive = false
-	tween.interpolate_property(self, "color",
-		color, dead_color, 0.2, 
-		Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-	tween.start()
+	tween_color(dead_color)
 
 func set_alive():
 	alive = true
-	tween.interpolate_property(self, "color",
-		color, alive_color, 0.2, 
-		Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-	tween.start()
+	tween_color(alive_color)
 
 func switch_state():
 	if alive:
 		set_dead()
 	else:
 		set_alive()
+
+func tween_color(new_color : Color):
+	var tween := create_tween()\
+		.set_trans(Tween.TRANS_CUBIC)\
+		.set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "color", new_color, 0.2)
+	
+	var rotation_tween := create_tween()\
+		.set_trans(Tween.TRANS_LINEAR)\
+		.set_ease(Tween.EASE_IN)
+	# rotation_tween.tween_property(self, "rotation", deg2rad(-10), 0.05)
+	# rotation_tween.tween_property(self, "rotation", deg2rad(5), 0.05)
+	# rotation_tween.tween_property(self, "rotation", deg2rad(0), 0.05)
